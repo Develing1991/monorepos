0963f960ab6cdd76b826a2aab7a2da3cbd07eb09
Develing1991 <completed0728@gmail.com>
2023-04-13 08:04:24 +0900

    monorepo sample

---
+++ packages/main/index.js
@@ -0,0 +1,5 @@
+import { run } from "@Develing1991/second";
+import snakeCase from "just-snake-case";
+
+console.log(snakeCase("Hello from inside main - nuxt3"));
+console.log(run());
---
+++ packages/second/index.js
@@ -0,0 +1,6 @@
+import snakeCase from "just-snake-case";
+import kebabCase from "just-kebab-case";
+export function run() {
+  // console.log("Hello from inside main - storybook");
+  return kebabCase("Hello from inside main - storybook111");
+}
